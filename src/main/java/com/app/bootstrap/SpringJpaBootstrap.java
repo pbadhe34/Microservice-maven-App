268527038e7cef2d2c04be789da72048b7cdfc53
pbadhe34 <pbadhe34@gmail.com>
2022-04-11 17:37:13 +0530

    Product details updated

--- src/main/java/com/app/bootstrap/SpringJpaBootstrap.java
+++ src/main/java/com/app/bootstrap/SpringJpaBootstrap.java
@@ -33,7 +33,7 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
 
     private void loadProducts() {
         Product shirt = new Product();
-        shirt.setDescription("Spring Framework  Shirt");
+        shirt.setDescription("Spring Framework TEE Shirt");
         shirt.setPrice(new BigDecimal("18.95"));
         shirt.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_shirt-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg");
         shirt.setProductId("235268845711068308");
@@ -42,7 +42,7 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
         log.info("Saved Shirt - id: " + shirt.getId());
 
         Product mug = new Product();
-        mug.setDescription("Spring Framework Guru Mug");
+        mug.setDescription("Spring Data Guru Mug");
         mug.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_coffee_mug-r11e7694903c348e1a667dfd2f1474d95_x7j54_8byvr_512.jpg");
         mug.setProductId("168639393495335947");
         mug.setPrice(new BigDecimal("11.95"));
